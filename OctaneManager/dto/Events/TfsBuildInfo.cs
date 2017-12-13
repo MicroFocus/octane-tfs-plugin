26f4285886482b20d4f708e6fc40f953e9462198
Radislav Berkovich <radislav.berkovich@hpe.com>
2017-12-13 11:44:05 +0200

    fixing Junit test results

---
+++ OctaneManager/dto/Events/TfsBuildInfo.cs
@@ -0,0 +1,17 @@
+﻿
+namespace MicroFocus.Ci.Tfs.Octane.Dto.Events
+{
+	public class TfsBuildInfo
+	{
+		public string CollectionName { get; internal set; }
+		public string Project { get; internal set; }
+		public string BuildDefinitionId { get; internal set; }
+		public string BuildId { get; internal set; }
+		public string BuildName { get; internal set; }
+
+		public override string ToString()
+		{
+			return $"{CollectionName}.{Project}.{BuildDefinitionId}.{BuildId}.{BuildName}";
+		}
+	}
+}
