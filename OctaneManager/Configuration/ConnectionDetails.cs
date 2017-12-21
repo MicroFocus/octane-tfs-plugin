51220e745baf335fb0d570e3d378fa84d92c0381
radislavB <radislav77@gmail.com>
2017-12-21 11:54:12 +0200

    fix computing related commits (#17)

--- OctaneManager/Configuration/ConnectionDetails.cs
+++ OctaneManager/Configuration/ConnectionDetails.cs
@@ -1,59 +1,67 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Hpe.Nga.Api.Core.Entities;
 using Newtonsoft.Json;
 
 namespace MicroFocus.Ci.Tfs.Octane.Configuration
 {
-    public class ConnectionDetails
-    {
-        [JsonIgnore]
-        public string Host
-        {
-            get
-            {
-                var index = WebAppUrl.IndexOf("/", WebAppUrl.LastIndexOf(":", StringComparison.Ordinal),
-                    StringComparison.Ordinal);
-                var res = WebAppUrl.Substring(0, index + 1);
-                return res;
-            }
-        }
+	public class ConnectionDetails
+	{
+		[JsonIgnore]
+		public string Host
+		{
+			get
+			{
+				var index = WebAppUrl.IndexOf("/", WebAppUrl.LastIndexOf(":", StringComparison.Ordinal),
+					StringComparison.Ordinal);
+				var res = WebAppUrl.Substring(0, index + 1);
+				return res;
+			}
+		}
 
-        public int SharedSpace
-        {
-            get
-            {
-                var index = WebAppUrl.IndexOf("=", StringComparison.Ordinal);
-                var res = int.Parse(WebAppUrl.Substring(index + 1));
-                return res;
-            }
-        }
+		public int SharedSpace
+		{
+			get
+			{
+				try
+				{
+					var index = WebAppUrl.IndexOf("=", StringComparison.Ordinal);
+					var sharedSpaceStr = WebAppUrl.Substring(index + 1);
+					var endIndex = sharedSpaceStr.IndexOf("/", StringComparison.Ordinal);
+					if (endIndex > -1)
+					{
+						sharedSpaceStr = sharedSpaceStr.Substring(0, endIndex);
+					}
+					var res = int.Parse(sharedSpaceStr);
+					return res;
+				}
+				catch (Exception)
+				{
+					throw new Exception($"Sharedspace id is expected, but not received in {WebAppUrl}");
+				}
+			}
+		}
 
-        public string WebAppUrl { get; set; }
-        public string ClientId { get; set; }
-        public string ClientSecret { get; set; }
+		public string WebAppUrl { get; set; }
+		public string ClientId { get; set; }
+		public string ClientSecret { get; set; }
 
-        public string Pat { get; set; }
+		public string Pat { get; set; }
 
-        public string TfsLocation { get; set; }
-        public ConnectionDetails()
-        {
+		public string TfsLocation { get; set; }
+		public ConnectionDetails()
+		{
 
-        }
+		}
 
-        public ConnectionDetails(string webAppUrl, string clientId, string clientSecret,string tfsLocation , Guid? instanceId = null)
-        {
-            WebAppUrl = webAppUrl;
-            ClientId = clientId;
-            ClientSecret = clientSecret;
-            InstanceId = instanceId ?? Guid.NewGuid();
-            TfsLocation = tfsLocation;
-        }
+		public ConnectionDetails(string webAppUrl, string clientId, string clientSecret, string tfsLocation, Guid? instanceId = null)
+		{
+			WebAppUrl = webAppUrl;
+			ClientId = clientId;
+			ClientSecret = clientSecret;
+			InstanceId = instanceId ?? Guid.NewGuid();
+			TfsLocation = tfsLocation;
+		}
 
-        public Guid InstanceId { get; set; } = Guid.NewGuid();
+		public Guid InstanceId { get; set; } = Guid.NewGuid();
 
-    }
+	}
 }
