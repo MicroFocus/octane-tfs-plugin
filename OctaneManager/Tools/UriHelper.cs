1a21636228cefbe91d52d3e31c0ab2c9823bad1d
Radislav Berkovich <radislav.berkovich@hpe.com>
2017-12-21 21:27:59 +0200

    remove dependencies

--- OctaneManager/Tools/UriHelper.cs
+++ OctaneManager/Tools/UriHelper.cs
@@ -1,9 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.ServiceModel.Channels;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace MicroFocus.Ci.Tfs.Octane.Tools
 {
