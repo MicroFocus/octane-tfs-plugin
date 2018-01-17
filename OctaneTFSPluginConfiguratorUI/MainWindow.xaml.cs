f1f1cf93cdb0dbfbdd2e08003b0a327e75f2488c
Michael Seldin <michael.seldin@hpe.com>
2018-01-18 01:10:30 +0200

    add configuration ui remove uneeded fields from setup

---
+++ OctaneTFSPluginConfiguratorUI/MainWindow.xaml.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MicroFocus.Adm.Octane.CiPlugins.Tfs.Core.Configuration;
+using MicroFocus.Adm.Octane.CiPlugins.Tfs.Core.Tools;
+
+namespace OctaneTFSPluginConfiguratorUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ConnectionDetails _conDetails = new ConnectionDetails();
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            if (!ConfigurationManager.ConfigurationExists())
+            {
+                InstanceId.Text = Guid.NewGuid().ToString();
+                TfsLocation.Text = ConnectionCreator.GetTfsLocationFromHostName();
+                return;                
+            }
+
+            var connectionDetails = ConfigurationManager.Read(false);
+
+            Location.Text = connectionDetails.ALMOctaneUrl;
+            ClientId.Text = connectionDetails.ClientId;
+            ClientSecret.Text = connectionDetails.ClientSecret;
+            TfsLocation.Text = connectionDetails.TfsLocation;
+            Pat.Text = connectionDetails.Pat;
+            InstanceId.Text = connectionDetails.InstanceId;            
+        }
+
+        private void TestConnectionButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            ReadFields();
+            try
+            {           
+                
+                ConnectionCreator.CheckMissingValues(_conDetails);
+                ConnectionCreator.CreateTfsConnection(_conDetails);
+                ConnectionCreator.CreateOctaneConnection(_conDetails);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            MessageBox.Show("Connection succesfull");
+
+        }
+
+
+        private void SaveButton_OnClick(object sender, RoutedEventArgs e)
+        {                   
+           ReadFields();
+           ConfigurationManager.WriteConfig(_conDetails);
+
+           MessageBox.Show("Settings saved!");
+        }
+
+        private void ReadFields()
+        {
+            var octaneServerUrl = Location.Text;
+            var clientId = ClientId.Text;
+            var clientSecret = ClientSecret.Text;
+            var instanceId = InstanceId.Text;
+            var pat = Pat.Text;
+            var tfsLocation = TfsLocation.Text;
+
+            var conDetails =
+                new ConnectionDetails(octaneServerUrl, clientId, clientSecret, tfsLocation, instanceId)
+                {
+                    Pat = pat
+                };
+
+            _conDetails = conDetails;
+        }
+    }
+}
