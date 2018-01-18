49310c151d2d9bf9833df13304c5d4baa4505b44
Michael Seldin <michael.seldin@hpe.com>
2018-01-18 11:36:47 +0200

    add Helper.cs

---
+++ OctaneTFSPluginConfiguratorUI/Helper.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OctaneTFSPluginConfiguratorUI
+{
+    class Helper
+    {
+
+        public static bool CheckedConnection { get; set; }
+    }
+}
