854be50e0b325bc794d582ca3992332a3395312a
Michael Seldin <michael.seldin@hpe.com>
2018-02-06 10:38:22 +0200

    try fix "ERROR: Unrecoverable build error" during setup build

--- package.cmd
+++ package.cmd
@@ -1,2 +1,3 @@
+regsvr32 ole32.dll
 "%DEVENV_LOCATION%" .\OctaneTfsPluginSetup\OctaneTfsPluginSetup.vdproj /build PackageDebug
 "%DEVENV_LOCATION%" .\OctaneTfsPluginSetup\OctaneTfsPluginSetup.vdproj /build PackageRelease
