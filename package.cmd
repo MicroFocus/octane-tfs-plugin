c7a0c89272086f7ca3f3fe785d96fa169ba0f108
Radislav Berkovich <radislav.berkovich@hpe.com>
2018-12-02 11:43:10 +0200

    tech : replace package.cmd

--- package.cmd
+++ package.cmd
@@ -1,7 +1,28 @@
-REM Run package and generate setups
+@echo off 
 regsvr32 /s ole32.dll
-call target2015.cmd
-echo build msi
+
+mkdir .\tfs-server-dlls\current
+SET installerProjectFile=.\OctaneTfsPluginSetup\OctaneTfsPluginSetup.vdproj
+SET regexToFind="\"DefaultLocation\" = \"8:.*\""
+
+ECHO **********************************************************
+ECHO *******************BUILD  2015****************************
+ECHO **********************************************************
+
+copy /Y .\tfs-server-dlls\2015 .\tfs-server-dlls\current
+
+SET replacement="\"DefaultLocation\" = \"8:[ProgramFiles64Folder]\\Microsoft Team Foundation Server 14.0\\Application Tier\\TFSJobAgent\\Plugins\""
+powershell -file replaceInFile.ps1 %installerProjectFile% %regexToFind%  %replacement% 
 "%DEVENV_LOCATION%" .\OctaneTfsPluginSetup\OctaneTfsPluginSetup.vdproj /build Package2015
-call target2017.cmd
+
+
+ECHO **********************************************************
+ECHO *******************BUILD  2017****************************
+ECHO **********************************************************
+copy /Y .\tfs-server-dlls\2017 .\tfs-server-dlls\current
+
+SET replacement="\"DefaultLocation\" = \"8:[ProgramFiles64Folder]\\Microsoft Team Foundation Server 15.0\\Application Tier\\TFSJobAgent\\Plugins\""
+powershell -file replaceInFile.ps1 %installerProjectFile% %regexToFind%  %replacement% 
 "%DEVENV_LOCATION%" .\OctaneTfsPluginSetup\OctaneTfsPluginSetup.vdproj /build Package2017
+
+ECHO ***********************DONE*******************************
