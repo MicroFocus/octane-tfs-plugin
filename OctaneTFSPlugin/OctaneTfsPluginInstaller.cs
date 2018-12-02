b6c9a64868f3020f865e54cabd0e38677fe6c25a
Radislav Berkovich <radislav.berkovich@hpe.com>
2018-12-02 13:38:21 +0200

    refactoring installer

--- OctaneTFSPlugin/OctaneTfsPluginInstaller.cs
+++ OctaneTFSPlugin/OctaneTfsPluginInstaller.cs
@@ -13,104 +13,216 @@
 * See the License for the specific language governing permissions and
 * limitations under the License.
 */
+using log4net;
+using log4net.Appender;
+using log4net.Core;
+using log4net.Layout;
+using log4net.Repository.Hierarchy;
+using MicroFocus.Adm.Octane.CiPlugins.Tfs.Core.Tools;
 using System;
 using System.Collections;
 using System.ComponentModel;
 using System.Diagnostics;
 using System.IO;
+using System.Reflection;
 using System.ServiceProcess;
-using System.Windows.Forms;
-using MicroFocus.Adm.Octane.CiPlugins.Tfs.Core.Configuration;
-using MicroFocus.Adm.Octane.CiPlugins.Tfs.Core.Tools;
-using MicroFocus.Adm.Octane.CiPlugins.Tfs.Core.Tools.Connectivity;
 
 namespace MicroFocus.Adm.Octane.CiPlugins.Tfs.Plugin
 {
     [RunInstaller(true)]
     public partial class OctaneTfsPluginInstaller : System.Configuration.Install.Installer
     {
+        protected static readonly ILog Log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+
         public OctaneTfsPluginInstaller()
         {
             InitializeComponent();
+            SetupLog4NetForSetup();
         }
 
 
         protected override void OnAfterInstall(IDictionary savedState)
         {
+            LogMessage("OnAfterInstall", EventLogEntryType.Information);
+
             base.OnAfterInstall(savedState);
 
-            LogUtils.WriteWindowsEvent("Starting service...", EventLogEntryType.Information, "ALM Octane Setup");
+            //DoNamespaceReservation();
+
+            StartTfsJobAgentService();
+
+            StartConfigurator();
+        }
+
+        private static void StartTfsJobAgentService()
+        {
+            LogMessage("Starting TFSJobAgent service...", EventLogEntryType.Information);
             var sc = new ServiceController("TFSJobAgent");
             sc.Start();
             sc.WaitForStatus(ServiceControllerStatus.Running);
-
-            var instpath = this.Context.Parameters["targetdir"];
-            
-            var path= Path.Combine(instpath, "ALMOctaneTFSPluginConfiguratorUI.exe");
-            System.Diagnostics.Process.Start(path);
-
-            var process = new System.Diagnostics.Process();
-            var startInfo =
-                new System.Diagnostics.ProcessStartInfo
-                {
-                    WindowStyle = System.Diagnostics.ProcessWindowStyle.Normal,
-                    FileName = "cmd.exe",
-                    Arguments = "/C netsh http add urlacl url=http://+:4567// user=Everyone",
-                    RedirectStandardOutput = true,
-                    UseShellExecute = false
-                };
-            process.StartInfo = startInfo;
-            process.Start();
-
-            var output = process.StandardOutput.ReadToEnd();
-
-            if (!output.Contains("reservation successfully added"))
-            {
-                LogUtils.WriteWindowsEvent("error adding reservation, restserver will not work!", EventLogEntryType.Error, "ALM Octane Setup");
-            }
-
-            LogUtils.WriteWindowsEvent("Reservation for rest server succesfully added ", EventLogEntryType.Information, "ALM Octane Setup");
         }
 
-        protected override void OnBeforeUninstall(IDictionary savedState)
+        private static void StopTfsJobAgentService()
         {
-            base.OnBeforeUninstall(savedState);
-
-            LogUtils.WriteWindowsEvent("Stopping service...", EventLogEntryType.Information, "ALM Octane Setup");
+            LogMessage("Stopping TFSJobAgent service...", EventLogEntryType.Information);
             var sc = new ServiceController("TFSJobAgent");
             if (sc.Status == ServiceControllerStatus.Running)
             {
                 sc.Stop();
                 sc.WaitForStatus(ServiceControllerStatus.Stopped);
             }
+        }
+
+        private void StartConfigurator()
+        {
+            var instpath = this.Context.Parameters["targetdir"];
+
+            //start configurator
+            var path = Path.Combine(instpath, "ALMOctaneTFSPluginConfiguratorUI.exe");
+            Process.Start(path);
+        }
+
+        private static void DoNamespaceReservation()
+        {
+            LogMessage("Starting NamespaceReservation ...", EventLogEntryType.Information);
+            //see all reservations : netsh http show urlacl
+            //delete reservation   : netsh http delete urlacl url = http://+:4567/
+            //add reservation      : netsh http add urlacl url = http://+:4567/ user=Everyone
+
+            String msg = "";
+            String command = "netsh";
+            try
+            {
+                string output = ExecuteCommand(command, "http show urlacl");
+                string url = "http://+:4567/";
+                if (!output.Contains(url))
+                {
+                    String addCommand = $"http add urlacl url={url} user=Everyone";
+                    output = ExecuteCommand(command, addCommand);
+                    if (!output.Contains("reservation successfully added"))
+                    {
+                        msg = $"Error adding Namespace reservation, restserver will not work!. Error = {output}";
+                        LogMessage(msg, EventLogEntryType.Error);
+                    }
+                    else
+                    {
+                        msg = $"Namespace reservation for rest server succesfully added  to {url}";
+                        LogMessage(msg, EventLogEntryType.Information);
+                    }
+                }
+                else
+                {
+                    msg = $"Namespace Reservation already exist to {url}";
+                    LogMessage(msg, EventLogEntryType.Warning);
+                    return;
+                }
+            }
+            catch (Exception e)
+            {
+                msg = $"Failed to do DoNamespaceReservation :  {e.Message}";
+                LogMessage(msg, EventLogEntryType.Information);
+            }
+
+        }
+
+        private static void LogMessage(String msg, EventLogEntryType eventLogEntryType)
+        {
+            LogUtils.WriteWindowsEvent(msg, eventLogEntryType, "ALM Octane Setup");
+
+            switch (eventLogEntryType)
+            {
+                case EventLogEntryType.Error:
+                    Log.Error(msg);
+                    break;
+                case EventLogEntryType.Warning:
+                    Log.Warn(msg);
+                    break;
+                default:
+                    Log.Warn(msg);
+                    break;
+            }
+        }
+
+        private static string ExecuteCommand(string command, string argument)
+        {
+            var process = new Process();
+            var startInfo =
+                new ProcessStartInfo
+                {
+                    WindowStyle = ProcessWindowStyle.Hidden,
+                    FileName = command,
+                    //Verb = "runas",
+                    Arguments = argument,
+                    RedirectStandardOutput = true,
+                    CreateNoWindow = true,
+                    UseShellExecute = false
+                };
+            process.StartInfo = startInfo;
+            process.Start();
+            process.WaitForExit();
+
+            var output = process.StandardOutput.ReadToEnd();
+
+            return output;
+        }
+
+
+        protected override void OnBeforeUninstall(IDictionary savedState)
+        {
+            LogMessage("OnBeforeUninstall", EventLogEntryType.Information);
+
+            base.OnBeforeUninstall(savedState);
+
+            StopTfsJobAgentService();
 
         }
 
         protected override void OnAfterUninstall(IDictionary savedState)
         {
+            LogMessage("OnBeforeUninstall", EventLogEntryType.Information);
+
             base.OnAfterUninstall(savedState);
 
-            LogUtils.WriteWindowsEvent("Starting service...", EventLogEntryType.Information, "ALM Octane Setup");
-            var sc = new ServiceController("TFSJobAgent");
-            sc.Start();
-            sc.WaitForStatus(ServiceControllerStatus.Running);
-
+            StartTfsJobAgentService();
         }
 
         protected override void OnBeforeInstall(IDictionary savedState)
         {
+            LogMessage("OnBeforeUninstall", EventLogEntryType.Information);
+
             base.OnBeforeInstall(savedState);
-            LogUtils.WriteWindowsEvent("Stopping service...", EventLogEntryType.Information,"ALM Octane Setup");
-            var sc = new ServiceController("TFSJobAgent");
-            if (sc.Status == ServiceControllerStatus.Running)
-            {
-                sc.Stop();
-                sc.WaitForStatus(ServiceControllerStatus.Stopped);
-            }
+
+            StopTfsJobAgentService();
         }
 
-        
-        
+        public static void SetupLog4NetForSetup()
+        {
+            String path = @"C:\Users\Public\Documents\OctaneTfsPlugin\logs\Setup.log";
+            Directory.CreateDirectory(Path.GetDirectoryName(path));
 
+            Hierarchy hierarchy = (Hierarchy)LogManager.GetRepository();
+
+            PatternLayout patternLayout = new PatternLayout();
+            patternLayout.ConversionPattern = "%date [%thread] %-5level %logger - %message%newline";
+            patternLayout.ActivateOptions();
+
+            RollingFileAppender roller = new RollingFileAppender();
+            roller.AppendToFile = true;
+            roller.File = path;
+            roller.Layout = patternLayout;
+            roller.MaxSizeRollBackups = 2;
+            roller.MaximumFileSize = "3MB";
+            roller.RollingStyle = RollingFileAppender.RollingMode.Size;
+            roller.StaticLogFileName = true;
+            roller.ActivateOptions();
+            hierarchy.Root.AddAppender(roller);
+
+            MemoryAppender memory = new MemoryAppender();
+            memory.ActivateOptions();
+            hierarchy.Root.AddAppender(memory);
+
+            hierarchy.Root.Level = Level.Debug;
+            hierarchy.Configured = true;
+        }
     }
 }
