afa64ba96a2090dd92227dc76b26bef1e706f983
Radislav Berkovich <radislav.berkovich@microfocus.com>
2019-01-09 09:43:52 +0200

    update package cmd

--- package.cmd
+++ package.cmd
@@ -3,16 +3,23 @@ regsvr32 /s ole32.dll
 
 mkdir .\tfs-server-dlls\current
 SET installerProjectFile=.\OctaneTfsPluginSetup\OctaneTfsPluginSetup.vdproj
-SET regexToFind="\"DefaultLocation\" = \"8:.*\""
+SET tfsVersionFile=.\OctaneManager\Tools\RunModeManager.cs
+SET installPathRegexToFind="\"DefaultLocation\" = \"8:.*\""
+SET tfsVersionRegexToFind="TfsVersionEnum.Tfs20.+;"
 
 ECHO **********************************************************
 ECHO *******************BUILD  2015****************************
 ECHO **********************************************************
 
 copy /Y .\tfs-server-dlls\2015 .\tfs-server-dlls\current
+SET installPathReplacement="\"DefaultLocation\" = \"8:[ProgramFiles64Folder]\\Microsoft Team Foundation Server 14.0\\Application Tier\\TFSJobAgent\\Plugins\""
+powershell -file replaceInFile.ps1 %installerProjectFile% %installPathRegexToFind% %installPathReplacement% 
+ECHO update install path to %installPathReplacement% 
 
-SET replacement="\"DefaultLocation\" = \"8:[ProgramFiles64Folder]\\Microsoft Team Foundation Server 14.0\\Application Tier\\TFSJobAgent\\Plugins\""
-powershell -file replaceInFile.ps1 %installerProjectFile% %regexToFind%  %replacement% 
+SET tfsVersionReplacement="TfsVersionEnum.Tfs2015;"
+powershell -file replaceInFile.ps1 %tfsVersionFile% %tfsVersionRegexToFind% %tfsVersionReplacement% 
+ECHO update tfs version to %tfsVersionReplacement% 
+
 "%DEVENV_LOCATION%" .\OctaneTfsPluginSetup\OctaneTfsPluginSetup.vdproj /build Package2015
 
 
@@ -21,8 +28,14 @@ ECHO *******************BUILD  2017****************************
 ECHO **********************************************************
 copy /Y .\tfs-server-dlls\2017 .\tfs-server-dlls\current
 
-SET replacement="\"DefaultLocation\" = \"8:[ProgramFiles64Folder]\\Microsoft Team Foundation Server 15.0\\Application Tier\\TFSJobAgent\\Plugins\""
-powershell -file replaceInFile.ps1 %installerProjectFile% %regexToFind%  %replacement% 
+SET installPathReplacement="\"DefaultLocation\" = \"8:[ProgramFiles64Folder]\\Microsoft Team Foundation Server 15.0\\Application Tier\\TFSJobAgent\\Plugins\""
+powershell -file replaceInFile.ps1 %installerProjectFile% %installPathRegexToFind% %installPathReplacement%
+ECHO update install path to %installPathReplacement% 
+
+SET tfsVersionReplacement="TfsVersionEnum.Tfs2017;"
+powershell -file replaceInFile.ps1 %tfsVersionFile% %tfsVersionRegexToFind% %tfsVersionReplacement% 
+ECHO update tfs version to %tfsVersionReplacement% 
+
 "%DEVENV_LOCATION%" .\OctaneTfsPluginSetup\OctaneTfsPluginSetup.vdproj /build Package2017
 
 ECHO ***********************DONE*******************************
