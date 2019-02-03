4f32eda4937b8cdd45a33a89d12b56e12c656280
Radislav Berkovich <radislav.berkovich@microfocus.com>
2019-02-03 08:52:11 +0200

    tfs 2018

--- package.cmd
+++ package.cmd
@@ -33,7 +33,6 @@ REM build setup files
 "%DEVENV_LOCATION%" .\OctaneTfsPluginSetup\OctaneTfsPluginSetup.vdproj /build Package2015
 
 
-
 ECHO **********************************************************
 ECHO *******************BUILD  2017****************************
 ECHO **********************************************************
@@ -56,31 +55,6 @@ ECHO update tfs version to %tfsVersionReplacement%
 
 REM build setup files
 "%DEVENV_LOCATION%" .\OctaneTfsPluginSetup\OctaneTfsPluginSetup.vdproj /build Package2017
-
-
-
-ECHO **********************************************************
-ECHO *******************BUILD  2018****************************
-ECHO **********************************************************
-copy /Y .\tfs-server-dlls\2018 .\tfs-server-dlls\current
-
-REM update installation path in setup project
-SET installPathReplacement="\"DefaultLocation\" = \"8:[ProgramFiles64Folder]\\Microsoft Team Foundation Server 2018\\Application Tier\\TFSJobAgent\\Plugins\""
-powershell -file replaceInFile.ps1 %installerProjectFile% %installPathRegexToFind% %installPathReplacement%
-ECHO update install path to %installPathReplacement% 
-
-REM update setup title with correct version of tfs
-SET installProductNameReplacement="\"ProductName\" = \"8:TFS 2018 Plugin for ALM Octane"
-powershell -file replaceInFile.ps1 %installerProjectFile% %installProductNameRegex% %installProductNameReplacement%
-ECHO update product name in installer to %installProductNameReplacement% 
-
-REM update tfs version in RunModeManager.cs
-SET tfsVersionReplacement="TfsVersionEnum.Tfs2018;"
-powershell -file replaceInFile.ps1 %tfsVersionFile% %tfsVersionRegexToFind% %tfsVersionReplacement% 
-ECHO update tfs version to %tfsVersionReplacement% 
-
-REM build setup files
-"%DEVENV_LOCATION%" .\OctaneTfsPluginSetup\OctaneTfsPluginSetup.vdproj /build Package2018
 
 
 ECHO ***********************DONE*******************************
