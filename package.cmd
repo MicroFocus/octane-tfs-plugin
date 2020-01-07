b0b77e6043b6dc0de44ce1c442e2ac7aab9392ba
Radislav Berkovich <radislav.berkovich@microfocus.com>
2020-01-07 17:05:55 +0200

    update package

--- package.cmd
+++ package.cmd
@@ -7,63 +7,6 @@ SET installProductNameRegex="\"ProductName\" = \"8:.*\""
 SET tfsVersionRegexToFind="TfsVersionEnum.Tfs20.+;"
 
 ECHO **********************************************************
-ECHO *******************BUILD  2015****************************
-ECHO **********************************************************
-
-REM update installation path in setup project
-copy /Y .\tfs-server-dlls\2015 .\tfs-server-dlls\current
-SET installPathReplacement="\"DefaultLocation\" = \"8:[ProgramFiles64Folder]\\Microsoft Team Foundation Server 14.0\\Application Tier\\TFSJobAgent\\Plugins\""
-powershell -file replaceInFile.ps1 %installerProjectFile% %installPathRegexToFind% %installPathReplacement% 
-ECHO update install path to %installPathReplacement% 
-
-REM update setup title with correct version of tfs
-SET installProductNameReplacement="\"ProductName\" = \"8:ALM Octane CI Plugin for TFS 2015 (v1.3)"\"
-powershell -file replaceInFile.ps1 %installerProjectFile% %installProductNameRegex% %installProductNameReplacement%
-ECHO update product name in installer to %installProductNameReplacement% 
-
-REM build setup files
-"%DEVENV_LOCATION%" .\OctaneTfsPluginSetup\OctaneTfsPluginSetup.vdproj /build Package2015
-
-
-ECHO **********************************************************
-ECHO *******************BUILD  2017****************************
-ECHO **********************************************************
-copy /Y .\tfs-server-dlls\2017 .\tfs-server-dlls\current
-
-REM update installation path in setup project
-SET installPathReplacement="\"DefaultLocation\" = \"8:[ProgramFiles64Folder]\\Microsoft Team Foundation Server 15.0\\Application Tier\\TFSJobAgent\\Plugins\""
-powershell -file replaceInFile.ps1 %installerProjectFile% %installPathRegexToFind% %installPathReplacement%
-ECHO update install path to %installPathReplacement% 
-
-REM update setup title with correct version of tfs
-SET installProductNameReplacement="\"ProductName\" = \"8:ALM Octane CI Plugin for TFS 2017 (v1.3)"\"
-powershell -file replaceInFile.ps1 %installerProjectFile% %installProductNameRegex% %installProductNameReplacement%
-ECHO update product name in installer to %installProductNameReplacement% 
-
-REM build setup files
-"%DEVENV_LOCATION%" .\OctaneTfsPluginSetup\OctaneTfsPluginSetup.vdproj /build Package2017
-
-
-ECHO **********************************************************
-ECHO *******************BUILD  2018****************************
-ECHO **********************************************************
-copy /Y .\tfs-server-dlls\2018 .\tfs-server-dlls\current
-
-REM update installation path in setup project
-SET installPathReplacement="\"DefaultLocation\" = \"8:[ProgramFiles64Folder]\\Microsoft Team Foundation Server 2018\\Application Tier\\TFSJobAgent\\Plugins\""
-powershell -file replaceInFile.ps1 %installerProjectFile% %installPathRegexToFind% %installPathReplacement%
-ECHO update install path to %installPathReplacement% 
-
-REM update setup title with correct version of tfs
-SET installProductNameReplacement="\"ProductName\" = \"8:ALM Octane CI Plugin for TFS 2018 (v1.3)"\"
-powershell -file replaceInFile.ps1 %installerProjectFile% %installProductNameRegex% %installProductNameReplacement%
-ECHO update product name in installer to %installProductNameReplacement% 
-
-REM build setup files
-"%DEVENV_LOCATION%" .\OctaneTfsPluginSetup\OctaneTfsPluginSetup.vdproj /build Package2018
-
-
-ECHO **********************************************************
 ECHO *******************BUILD  2019****************************
 ECHO **********************************************************
 copy /Y .\tfs-server-dlls\2019 .\tfs-server-dlls\current
@@ -74,7 +17,7 @@ powershell -file replaceInFile.ps1 %installerProjectFile% %installPathRegexToFin
 ECHO update install path to %installPathReplacement% 
 
 REM update setup title with correct version of tfs
-SET installProductNameReplacement="\"ProductName\" = \"8:ALM Octane CI Plugin for Azure DevOps Server 2019 (v1.3)"\"
+SET installProductNameReplacement="\"ProductName\" = \"8:ALM Octane CI Plugin v1.3 for Azure DevOps Server 2019\""
 powershell -file replaceInFile.ps1 %installerProjectFile% %installProductNameRegex% %installProductNameReplacement%
 ECHO update product name in installer to %installProductNameReplacement% 
 
