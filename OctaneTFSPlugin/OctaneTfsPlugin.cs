8c68ab6d5b6a3ebaf911d273f2c63ec6e2dd2e34
Radislav Berkovich <radislav.berkovich@microfocus.com>
2020-01-07 11:48:13 +0200

    removing reference to Build2.WebApi.dll

--- OctaneTFSPlugin/OctaneTfsPlugin.cs
+++ OctaneTFSPlugin/OctaneTfsPlugin.cs
@@ -24,7 +24,6 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Reflection;
-using System.Linq;
 using MicroFocus.Adm.Octane.CiPlugins.Tfs.Core.Tools.Connectivity;
 
 namespace MicroFocus.Adm.Octane.CiPlugins.Tfs.Plugin
